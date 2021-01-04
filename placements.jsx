758487dcbf60f109ad2f2c54bb8deecc7b309e1e
rohithsai1904 <70425960+rohithsai1904@users.noreply.github.com>
2021-01-04 18:25:13 +0530

    Create placements.jsx

---
+++ placements.jsx
@@ -0,0 +1,37 @@
+<div id="platab">
+               <ul className="plat">
+			     <li><a href="#">2020</a></li>
+				 <li><a href="#">2019</a></li>
+				 <li><a href="#">2018</a></li>
+				 <li><a href="#">2017</a></li>
+				 <li><a href="#">2016</a></li>
+				 <li><a href="#">2015</a></li>
+				 <li><a href="#">2014</a></li>
+				 <li><a href="#">2013</a></li>
+				 <li><a href="#">2012</a></li>
+				 <li><a href="#">2011</a></li>
+			   </ul>
+            </div>
+            
+            
+  //styles for the placements//
+  #platab
+{
+	float:left;
+	background-color:rgb(10, 8, 66);
+	font-size: 18px;
+	padding: 15px 5px;
+	border-radius:0px 18px 18px 0px;
+	margin: 10px;
+}
+ul.plat{
+    color: white;
+	list-style-type:circle;
+}
+ul li{
+    display: list-item;
+    text-align: -webkit-match-parent;
+	padding: 8px;
+	border : 2px solid rgb(10, 8, 66);
+	margin : 10px;
+}
